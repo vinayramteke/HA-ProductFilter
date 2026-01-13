fa061b3861f2c207d9fe4a1890ca83aa868f7ee2
vinayramteke <22bee120@nith.ac.in>
2026-01-13 15:57:10 +0530

    Refactor Table component to display product details in a structured table format

--- product-filter/src/cpmponents/Table.jsx
+++ product-filter/src/cpmponents/Table.jsx
@@ -1,7 +1,24 @@
 const Table = () => {
   return (
-    <div>
-      <h2>I'm Table.</h2>
+    <div className="table-body">
+      <table>
+        <thead>
+          <tr>
+            <th>Title</th>
+            <th>Brand</th>
+            <th>Category</th>
+            <th>Prince</th>
+            <th>Rating</th>
+          </tr>
+        </thead>
+        <tbody>
+          <td>Motor</td>
+          <td>mkm</td>
+          <td>bus</td>
+          <td>$6789</td>
+          <td>4.5/5</td>
+        </tbody>
+      </table>
     </div>
   );
 };
